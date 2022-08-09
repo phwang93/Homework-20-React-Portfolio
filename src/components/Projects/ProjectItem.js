126fa9f995bd7be03f8a33fc3d60e86d6b2c35d7
phwang93 <98869145+phwang93@users.noreply.github.com>
2022-08-09 13:00:16 -0700

    changing css

--- src/components/Projects/ProjectItem.js
+++ src/components/Projects/ProjectItem.js
@@ -15,8 +15,8 @@ function ProjectItem(props) {
         <div className="middle">
           <p className="text-name">{props.name}</p>
           <p className="text-tech">{props.tech}</p>
-          <a href={props.link}><button className="btn btn-primary btn-livelink">Visit website</button></a>
-          <a href={props.github}><button className="btn btn-primary btn-github">See the code</button></a>
+          <a href={props.link}><button className="btn btn-primary btn-livelink">Deployed</button></a>
+          <a href={props.github}><button className="btn btn-primary btn-github">Github</button></a>
         </div>
       </div>
     </div>

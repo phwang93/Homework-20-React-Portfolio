0c6d4d705a76c9396052c5bad203a290c8f799f1
phwang93 <98869145+phwang93@users.noreply.github.com>
2022-08-09 20:31:32 -0700

    Updates

---
+++ precache-manifest.3623f4882f174165fa030d5a3c17f83c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd7d35a2480a8d829497",
+    "url": "/Homework-20-React-Portfolio/static/css/main.617acc56.chunk.css"
+  },
+  {
+    "revision": "dd7d35a2480a8d829497",
+    "url": "/Homework-20-React-Portfolio/static/js/main.4c6d885a.chunk.js"
+  },
+  {
+    "revision": "bedee7e65051d6da681c",
+    "url": "/Homework-20-React-Portfolio/static/js/runtime~main.7a6d98e6.js"
+  },
+  {
+    "revision": "513cbc48c68bad539906",
+    "url": "/Homework-20-React-Portfolio/static/css/2.dd22d6a0.chunk.css"
+  },
+  {
+    "revision": "513cbc48c68bad539906",
+    "url": "/Homework-20-React-Portfolio/static/js/2.b17d011c.chunk.js"
+  },
+  {
+    "revision": "61c75ad1c6ad002fdd6f",
+    "url": "/Homework-20-React-Portfolio/static/js/3.a50d1155.chunk.js"
+  },
+  {
+    "revision": "5bce4a22ed924e85bd9a5ab6e5cf476c",
+    "url": "/Homework-20-React-Portfolio/static/media/Resume.5bce4a22.pdf"
+  },
+  {
+    "revision": "bb60c514b094ef5f5ecb645aac88cca8",
+    "url": "/Homework-20-React-Portfolio/static/media/me.bb60c514.PNG"
+  },
+  {
+    "revision": "3d3cb0eb0aebb234d7b05ee4ab1096ad",
+    "url": "/Homework-20-React-Portfolio/index.html"
+  }
+];
